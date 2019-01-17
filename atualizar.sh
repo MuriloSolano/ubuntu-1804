1b9619c3c4545a4b19f03233654da29ce9acf931
MuriloSolano <46789161+MuriloSolano@users.noreply.github.com>
2019-01-17 16:33:45 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,28 +6,38 @@ echo
 	sudo apt update 
 echo
 echo Listas Atualizadas	
+sleep 5
+clear
 
 echo Atualizando os Softwares 
 echo 
 	sudo apt -y upgrade 
 echo
-echo Softwares Atualizados	
+echo Softwares Atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo 
 	sudo apt -y dist-upgrade
 echo
 echo Kernel Atualizado	
+sleep 5
+clear
 
 echo Removendo Softwares 
 echo 
 	sudo apt -y autoremove
 echo
-echo Softwares Removidos	
+echo Softwares Removidos
+sleep 5
+clear
 
 echo Limpando o Cache
 echo
 	sudo apt -y autoclean
 echo
 echo Cache Limpo
+sleep 5
+clear
 	
